0a12877774b66102ef7855c0be8fafa81b03139f
Capucine Leclerc <capucine_49@hotmail.com>
2018-07-23 01:12:15 +0200

    config reach router and navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
+import Navbar from './components/Navbar.js'
+import Write from './container/Write.js'
+import { Router } from '@reach/router'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
+render() {
+return (
+  <div>
+  <Navbar />
+  <Router>
+    <Write path='/'>
+    </Write>
+    <Write remove path='/writings/remove'>
+    
+    </Write>
+      </Router>
+</div>
+)
+}
 }
 
 export default App
